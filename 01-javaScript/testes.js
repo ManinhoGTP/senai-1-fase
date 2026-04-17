592c9613e6847c082083e74428b891f0d8e25aad
ManinhoGTP <gabrieltuler99@gmail.com>
2026-04-16 22:11:47 -0300

    JavaScript

---
+++ 01-javaScript/testes.js
@@ -0,0 +1,17 @@
+// alert("Olá mundo!")
+// alert("Segunda menssagem")
+
+// let nome = "Gabriel"
+// let idade = 17
+
+// alert("Nome: " + nome)
+// alert("Idade: " + idade)
+// alert("Nome: " + nome + "\nIdade: " + idade)
+
+// nome = prompt("Digite seu nome: ")
+// alert("Nome digitado: " + nome)
+
+let nota1 
+nota1 = prompt("Informe sua nota: ")
+nota1 ++    
+alert(nota1)
