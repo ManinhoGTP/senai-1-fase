a00791aeb53af8f2d2463057b40a717d1cb96d5f
GABRIEL TULER PEREIRA <gabriel_pereira183@estudante.sesisenai.org.br>
2026-04-27 20:07:29 -0300

    Nada demais

---
+++ 02.javaScript/Junin.js
@@ -0,0 +1,12 @@
+//Variáveis
+let salario, dias, salarioDiario
+
+//Input
+salario = Number(prompt("Salário: "))
+dias = Number(prompt("Dias trabalhados: "))
+
+//Processar informações
+salarioDiario = salario / dias
+
+ //Output
+alert("Salário diário: R$ " + salarioDiario.toFixed(2))
