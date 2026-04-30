6fe8ac93405a4ece4e9c5f53b792a2d856b55199
GABRIEL TULER PEREIRA <gabriel_pereira183@estudante.sesisenai.org.br>
2026-04-30 20:33:24 -0300

    Atividades em javaScript

--- 03.css/script.js
+++ 03.css/script.js
@@ -13,5 +13,31 @@ function calcularPrecoBrique(){
     alert("Com base na política de preços de romero, a obra deve ser vendida à R$ " + valorRevenda.toFixed(2))
     console.log("Com base na política de preços de romero, a obra deve ser vendida à R$ " + valorRevenda.toFixed(2))
 
-    document.getElementById("resultado").innerHTML = "Valor revenda: " + valorRevenda.toFixed(2)
+    document.getElementById("resultadoRomero").innerHTML = "Valor revenda: " + valorRevenda.toFixed(2)
 }   
+function verificarProvisoes(){
+    //Variáveis
+    let NumMarujos, qntComida, comidaMinima
+
+    //Input
+    NumMarujos = Number(prompt("Quantia de marujos a bordo: "))
+    qntComida = Number(prompt("Kilos de comida: "))
+
+    //Processar
+    comidaMinima = NumMarujos * 1.5
+
+    //Output
+    if (comidaMinima > qntComida || NumMarujos < 10) {
+        document.getElementById("resultadoGanso").innerHTML = "Algo está errado. Posseidom não quer ninguém no mar hoje."
+            if (comidaMinima > qntComida) {
+                alert("Faltaram: " + (comidaMinima - qntComida) + " Kilos de comida")
+            }
+            if (NumMarujos < 10) {
+                alert("Faltaram: " + (10 - NumMarujos) + " Marujos!")
+            }
+    }else if (comidaMinima <= qntComida && NumMarujos >= 10) {
+
+        document.getElementById("resultadoGanso").innerHTML = "Provisões suficientes. Rumo ao horizonte!"
+
+    }
+}
