dd5f337709509450329ffe28e1153be8912a0d48
ManinhoGTP <gabriel_pereira183@estudante.sesisenai.org.br>
2026-05-19 20:13:58 -0300

    Nada demais por aqui

--- 04-Variáveis/script.js
+++ 04-Variáveis/script.js
@@ -19,61 +19,64 @@ function decrementarContagem(){
 //MJ Harry Ploter
 let valorTotalMJ = 0
 function adicionarVenda(){
-    let valoAdicionado = Number(document.getElementById("valorDaVenda").value)
+    let valorAdicionado = Number(document.getElementById("valorDaVenda").value)
     let output = document.getElementById("valorTotalMJ")
     
-    valorTotalMJ += valoAdicionado
+    document.getElementById("valorDaVenda").value = ""
+    valorTotalMJ += valorAdicionado
     output.innerHTML = valorTotalMJ.toFixed(2) + " R$"
+
 }
 
+
 //Copo de dados //Código merda não repare
-let numero = 0
+let pontos = 0
 let contD4 = 0, contD6 = 0, contD8 = 0, contD10 = 0, contD12 = 0, contD20 = 0
 let rolagens = document.getElementById("rolagens")
 function d4(){
     numeroSorteado = Math.ceil((Math.random()*4))
-    numero = Number(numero + numeroSorteado)
-    document.getElementById("pontos").innerHTML = "Pontos: " + numero
+    pontos = Number(pontos + numeroSorteado)
+    document.getElementById("pontos").innerHTML = "Pontos: " + pontos
     contD4 ++
     document.getElementById("d4Rolados").innerHTML = "d4 rolados: " + contD4
     rolagens.innerHTML += numeroSorteado + ", "
 }
 function d6(){
     numeroSorteado = Math.ceil((Math.random()*6))
-    numero = Number(numero + numeroSorteado)
-    document.getElementById("pontos").innerHTML = "Pontos: " + numero
+    pontos = Number(pontos + numeroSorteado)
+    document.getElementById("pontos").innerHTML = "Pontos: " + pontos
     contD6 ++
-    document.getElementById("d6Rolados").innerHTML = "d6 rolados" + contD6
+    document.getElementById("d6Rolados").innerHTML = "d6 rolados: " + contD6
     rolagens.innerHTML += numeroSorteado + ", "
 }
 function d8(){
     numeroSorteado = Math.ceil((Math.random()*8))
-    numero = Number(numero + numeroSorteado)
-    document.getElementById("pontos").innerHTML = "Pontos: " + numero
+    pontos = Number(pontos + numeroSorteado)
+    document.getElementById("pontos").innerHTML = "Pontos: " + pontos
     contD8 ++
     document.getElementById("d8Rolados").innerHTML = "d8 rolados: " + contD8
     rolagens.innerHTML += numeroSorteado + ", "
 }
 function d10(){
     numeroSorteado = Math.ceil((Math.random()*10))
-    numero = Number(numero + numeroSorteado)
-    document.getElementById("pontos").innerHTML = "Pontos: " + numero
+    pontos = Number(pontos + numeroSorteado)
+    document.getElementById("pontos").innerHTML = "Pontos: " + pontos
     contD10 ++
     document.getElementById("d10Rolados").innerHTML = "d10 rolados: " + contD10
     rolagens.innerHTML += numeroSorteado + ", "
 }
 function d12(){
     numeroSorteado = Math.ceil((Math.random()*12))
-    numero = Number(numero + numeroSorteado)
-    document.getElementById("pontos").innerHTML = "Pontos: " + numero
+    pontos = Number(pontos + numeroSorteado)
+    document.getElementById("pontos").innerHTML = "Pontos: " + pontos
     contD12 ++
     document.getElementById("d12Rolados").innerHTML = "d12 rolados: " + contD12
     rolagens.innerHTML += numeroSorteado + ", "
 }
 function d20(){
     numeroSorteado = Math.ceil((Math.random()*20))
-    numero = Number(numero + numeroSorteado)
-    document.getElementById("pontos").innerHTML = "Pontos: " + numero
+    pontos = Number(pontos + numeroSorteado)
+    document.getElementById("pontos").innerHTML = "Pontos: " + pontos
     contD20 ++
     document.getElementById("d20Rolados").innerHTML = "d20 rolados: " + contD20
     rolagens.innerHTML += numeroSorteado + ", "
---
+++ 99-testes/script.js
@@ -0,0 +1,7 @@
+
+let pi = 3.14159, raio, volume
+
+raio = Number(prompt("Raio:"))
+volume = Number((4/3.0) * pi * (raio**3))
+
+console.log("VOLUME = " + volume.toFixed(3))
