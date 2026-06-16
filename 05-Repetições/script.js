49bad41c3f05183971b9fc4c10b879870f6fad94
GABRIEL TULER PEREIRA <gabriel_pereira183@estudante.sesisenai.org.br>
2026-06-15 22:08:01 -0300

    repetição For

--- 05-Repetições/script.js
+++ 05-Repetições/script.js
@@ -1,10 +1,15 @@
 //While
-function contadorWhile(){
-let contador = 1
+function contadorWhile(contador, incremento){
     while(contador < 10){
         alert(`contador menor que 10 (${contador})`)
-        contador++
+        contador += incremento
     }
 }
 
-
+//For
+function contadorFor(){
+let numero = prompt("Escolha um número para mostrar sua tabuada")
+    for(let i = 1; i <= 10; i++){
+        alert(`${i} X ${numero} = ${numero * i}`)
+    }
+}
