8101d2e2f30e424ae61ab38b961932cd1b139909
GABRIEL TULER PEREIRA <gabriel_pereira183@estudante.sesisenai.org.br>
2026-07-06 22:18:32 -0300

    atividades

---
+++ 08-Arrays/oscar desafio indice/script.js
@@ -0,0 +1,42 @@
+const personagens = ["rivalado jesus", "Kowalski", "Genezio", "Waltdisney"]
+
+const lista = document.getElementById("lista")
+const seletor = document.getElementById("seletor")
+
+// personagens.forEach((nome, i) => {
+//     const celula = document.createElement("div")
+//     celula.className = "celula"
+//     celula.id = "item" + i
+//     celula.innerHTML = `<span class="valor">${nome}</span><class="idx">[${i}]</span>`
+//     lista.appendChild(celula)
+
+//     const opt = document.createElement("option")
+//     opt.value = nome
+//     opt.textContent = nome
+//     seletor.appendChild(opt)
+// })
+
+// function buscarIndice(){
+
+//     document.querySelectorAll(".celula").forEach(c.classLista.remove("destaque"))
+//     const nomeEscolhido = selector.value
+//     const indice = personagens.indexOf(nomeEscolhido)
+//     document.getElementById("resultado").textContent = `"${nomeEscolhido}" está no indice ${indice}`
+//     document.getElementById("item-" + indice).classList.add("destaque")
+
+// }
+
+personagens.forEach((nome, i) => {
+
+    const celula = document.createElement("div")
+    celula.className = "celula"
+    celula.id = "item" + i
+    celula.innerHTML = `<p>${nome}</p>`
+    lista.appendChild(celula)
+
+    const opt = document.createElement("option")
+    opt.value = nome
+    opt.textContent = nome
+    seletor.appendChild(opt)
+});
+
