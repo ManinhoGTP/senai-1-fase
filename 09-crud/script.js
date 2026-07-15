0d3356ee3d90ed136c093e68fe07c3ff7eb7bf2b
GABRIEL TULER PEREIRA <gabriel_pereira183@estudante.sesisenai.org.br>
2026-07-14 22:10:39 -0300

    treinando  GRUD

--- 09-crud/script.js
+++ 09-crud/script.js
@@ -14,7 +14,8 @@ const inputNome = document.getElementById("input-nome"), inputCor = document.get
 const inputAltura = document.getElementById("input-altura"), inputCusto = document.getElementById("input-custo") 
 
 //Array de dinos
-const dinos = []
+let dinos = []
+
 
 function resetarInputs(){
     //Resetar inputs
@@ -22,7 +23,49 @@ function resetarInputs(){
     inputCor.value = ""
     inputAltura.value = ""
     inputCusto.value = ""
+
+    inputNome.focus()
 }
+function testar(){
+    dinos = [{
+        id: 162162161,
+        nome: "teste1",
+        cor: "cor1",
+        altura: 1,
+        custo: 1
+    },
+    {
+        id: 6436346363,
+        nome: "teste2",
+        cor: "cor2",
+        altura: 2,
+        custo: 2
+    },
+    {
+        id: 83548537835,
+        nome: "teste3",
+        cor: "cor3",
+        altura: 3,
+        custo: 3
+    },
+    {
+        id: 5484537347347437,
+        nome: "teste4",
+        cor: "cor4",
+        altura: 4,
+        custo: 4
+    },
+    {
+        id: 7433838383,
+        nome: "teste5",
+        cor: "cor5",
+        altura: 5,
+        custo: 5
+    }
+    ]
+    console.log(dinos)
+ }
+
 
 function cadastrarDino(){
     //Dino a ser adicionado
@@ -34,10 +77,18 @@ function cadastrarDino(){
         custo: Number(inputCusto.value)
     }
 
+    
     //Adicionar ao array
     dinos.push(novoDino)
     //Resetar Input
     resetarInputs()
     //Console log
     console.log(dinos)
+}
+function mostrarTodosOsDinos(){
+    let lista = document.getElementById("listaDeDinos")
+
+    for(let i = 0; i<dinos.length; i++){
+        lista.innerHTML += `${i} - ${dinos[i].nome} <br>'`
+    }
 }
